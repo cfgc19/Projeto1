1224f63a405aeb7982b5775eba5f5df0fbc61438
cfgc19 <cfilipa.19@gmail.com>
2017-02-27 18:22:53 +0000

    alterei o teste

--- teste.java
+++ teste.java
@@ -3,10 +3,26 @@ import java.util.Scanner;
 public class teste {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 		System.out.println("ola:  ");
+
 		int e = scan.nextInt();
+
+		int ranting;
+
+		int opcao_certa =2;
+		switch (opcao_certa) {
+		case 1:
+			// pesquisa por instituicao
+			System.out.print("Escolha o valor minimo do Rating: ");
+			ranting = scan.nextInt();
+			break;
+		case 2:
+			System.out.print("Escolha o valor minimo do Rating: ");
+			break;
+		}
 		
+
 	}
 }
