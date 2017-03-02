44bf3c4c4478b4d97609dd25e513ad187dd81931
Adriana <adrianaifcosta.95@gmail.com>
2017-03-02 17:35:07 +0000

    criei um novo xml, com a estrutura a usar pelo processor

---
+++ main/Processor.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.Scanner;
+
+public class Processor {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Project selec_series=
+	}
+
+
+}
