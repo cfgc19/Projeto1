babbe95d0956f01d8ea17de42df29f2ad4705285
cfgc19 <cfilipa.19@gmail.com>
2017-03-06 11:40:30 +0000

    nao fiz nada

--- main/Processor.java
+++ main/Processor.java
@@ -3,6 +3,10 @@ package main;
 import java.io.File;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import generated.Actor;
@@ -16,20 +20,46 @@ public class Processor {
 		Actors actors = unmarshall.unmarshalles_actors();
 		Project project=unmarshall.unmarshalles_project();
 		String actor="";
+		String serie ="";
 		ArrayList<String> actors_list = new ArrayList<String>();
+		Map<String, ArrayList<String>> series_by_actors = new HashMap<String,ArrayList<String>>();
+		
+		ArrayList<String> series_names = new ArrayList<String>();
+		
+		if(series_by_actors.containsKey("Joana")){
+		    series_names = series_by_actors.get("Oliver");
+		    series_names.add("ola");
+		} else {
+		    series_names = new ArrayList<String>();
+		    series_names.add("oiii");
+		    series_by_actors.put("Joana", series_names);
+		}
+		
 		
 		//todos os atores
 		for (int i= project.getSerie().size()-1; i>=0;i--)
 		{
 			for (int j = project.getSerie().get(i).getCast().size()-1; j >= 0; j--) {
 				actor = project.getSerie().get(i).getCast().get(j).getName();
-				if (!actors_list.contains(actor)) {
-					actors_list.add(actor);
+				serie =project.getSerie().get(i).getSerieName();
+				if(series_by_actors.containsKey(actor)){
+				    series_names = series_by_actors.get(actor);
+				    series_names.add(serie);
+				    series_names.sort(String::compareToIgnoreCase);
+				} else {
+				    series_names = new ArrayList<String>();
+				    series_names.add(serie);
+				    series_names.sort(String::compareToIgnoreCase);
+				    series_by_actors.put(actor, series_names);
 				}
 			}
 		}
+		series_names.sort(String::compareToIgnoreCase);
+		System.out.println(series_by_actors);
+
 		
 		for (int i = actors.getActor().size()- 1; i >= 0; i--) {
+			
 			//BigInteger ranting1 = project.getSerie().get(i).getRating();
 				//if (ranting1.compareTo(ranting)>=0) {
 					//series_list.add(project.getSerie().get(i).getSerieName());
--- main/Selector.java
+++ main/Selector.java
@@ -3,6 +3,9 @@ package main;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Selector {
 
@@ -26,5 +29,28 @@ public class Selector {
 			System.out.println(result_list.get(i).toString());
 		}
 		
+		
+		Map<String, ArrayList<String>> series_by_actors = new HashMap<String,ArrayList<String>>();
+		String[] nomes_series1 = {"Sexy and the city", "asfd"};
+		ArrayList<String> series_names = new ArrayList<String>(Arrays.asList(nomes_series1));
+		
+		series_by_actors.put("Oliver", series_names);
+		ArrayList<String> list = new ArrayList<String>();
+		
+		if(series_by_actors.containsKey("Oliver")){
+		    // if the key has already been used,
+		    // we'll just grab the array list and add the value to it
+		    list = series_by_actors.get("Oliver");
+		    list.add("ola");
+		    list.sort(String::compareToIgnoreCase);
+		} else {
+		    // if the key hasn't been used yet,
+		    // we'll create a new ArrayList<String> object, add the value
+		    // and put it in the array list with the new key
+		    list = new ArrayList<String>();
+		    list.add("oiii");
+		    series_by_actors.put("Joana", list);
+		}
+		System.out.println(series_by_actors);
 	}
 }
