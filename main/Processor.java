995efb2c4553850ad28627f4f55b5d7595b199fb
cfgc19 <cfilipa.19@gmail.com>
2017-03-06 12:33:58 +0000

    cenas2

--- main/Processor.java
+++ main/Processor.java
@@ -22,16 +22,12 @@ public class Processor {
 	
 	public static HashMap ola (Map map){
 		 List list = new LinkedList(map.entrySet());
-	       // Defined Custom Comparator here
 	       Collections.sort(list, new Comparator() {
 	            public int compare(Object o1, Object o2) {
 	               return ((Comparable) ((Map.Entry) (o1)).getValue())
 	                  .compareTo(((Map.Entry) (o2)).getValue());
 	            }
 	       });
-
-	       // Here I am copying the sorted list in HashMap
-	       // using LinkedHashMap to preserve the insertion order
 	       HashMap sortedHashMap = new LinkedHashMap();
 	       for (Iterator it = list.iterator(); it.hasNext();) {
 	              Map.Entry entry = (Map.Entry) it.next();
