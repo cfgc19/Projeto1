11863246317e8ba8d53a1634f7ab2e8a14b7335c
cfgc19 <cfilipa.19@gmail.com>
2017-03-06 18:00:15 +0000

    conflitos no processor

--- main/Processor.java
+++ main/Processor.java
@@ -20,26 +20,20 @@ import generated1.Project;
 import generated1.Serie;
 
 public class Processor {
-	
-	public static void main (String[] args){
-		
-	}
-/*
-	
-	public static HashMap sortByValue (Map map){
-		 List list = new LinkedList(map.entrySet());
-		 Collections.sort(list, new Comparator() {
-			 public int compare(Object value1, Object value2) {
-	               return ((Comparable) ((Map.Entry) (value2)).getValue())
-	                  .compareTo(((Map.Entry) (value1)).getValue());
-	            }
-	       });
-		 HashMap sortedHashMap = new LinkedHashMap();
-		 for (Iterator it = list.iterator(); it.hasNext();) {
-	              Map.Entry entry = (Map.Entry) it.next();
-	              sortedHashMap.put(entry.getKey(), entry.getValue());
-	     } 
-	     return sortedHashMap;
+
+	public static HashMap sortByValue(Map map) {
+		List list = new LinkedList(map.entrySet());
+		Collections.sort(list, new Comparator() {
+			public int compare(Object value1, Object value2) {
+				return ((Comparable) ((Map.Entry) (value2)).getValue()).compareTo(((Map.Entry) (value1)).getValue());
+			}
+		});
+		HashMap sortedHashMap = new LinkedHashMap();
+		for (Iterator it = list.iterator(); it.hasNext();) {
+			Map.Entry entry = (Map.Entry) it.next();
+			sortedHashMap.put(entry.getKey(), entry.getValue());
+		}
+		return sortedHashMap;
 	}
 
 	public static void main(String[] args) {
@@ -53,38 +47,33 @@ public class Processor {
 		List<String> actors_list = new ArrayList<String>();
 		Map<String, List<String>> series_by_actors = new HashMap<String, List<String>>();
 		Map<String, BigInteger> episodes_by_actors = new HashMap<String, BigInteger>();
-		int t=0;
-		
-		
+		int t = 0;
+
 		List<String> series_names = new ArrayList<String>();
 		// todos os atores
 		BigInteger actors_episodes;
 		for (int i = project.getSerie().size() - 1; i >= 0; i--) {
-			//episodes = project.getSerie().get(i).getNumberOfEpisodes();
+			// episodes = project.getSerie().get(i).getNumberOfEpisodes();
 			serie = project.getSerie().get(i).getSerieName();
-<<<<<<< HEAD
-			//System.out.println(episodes);
-=======
->>>>>>> Filipa
 			for (int j = project.getSerie().get(i).getCast().size() - 1; j >= 0; j--) {
-				actor = project.getSerie().get(i).getCast().get(j).getName();				
-				if(series_by_actors.containsKey(actor)){
-				    series_names = series_by_actors.get(actor);
-				    actors_episodes=episodes_by_actors.get(actor);
-				    
-				//    actors_episodes.add(episodes);
-				    
-				    series_names.add(serie);
-				    series_names.sort(String::compareToIgnoreCase);
-			
+				actor = project.getSerie().get(i).getCast().get(j).getName();
+				if (series_by_actors.containsKey(actor)) {
+					series_names = series_by_actors.get(actor);
+					actors_episodes = episodes_by_actors.get(actor);
+
+					// actors_episodes.add(episodes);
+
+					series_names.add(serie);
+					series_names.sort(String::compareToIgnoreCase);
+
 				} else {
-					
+
 					series_names = new ArrayList<String>();
 					series_names.add(serie);
 
 					series_names.sort(String::compareToIgnoreCase);
 					series_by_actors.put(actor, series_names);
-					//episodes_by_actors.put(actor, episodes);
+					// episodes_by_actors.put(actor, episodes);
 					t++;
 				}
 			}
@@ -98,29 +87,19 @@ public class Processor {
 		for (Map.Entry<String, List<String>> entry : series_by_actors.entrySet()) {
 			String key = entry.getKey();
 			List<String> value = entry.getValue();
-<<<<<<< HEAD
-			actors.getActor().get(t).setActorName(key);
-			List<String> series_temporary_list = actors.getActor().get(t).getSerie();
-			series_temporary_list = value;
-			t++;
-		}
-		File xmlFile1 = new File("./src/actors.xml");
-		File schemaFile1 = new File("./src/actor.xsd");
-=======
 			Actor actor1 = new Actor();
 			actor1.setActorName(key);
-			List<String> series_list1 =actor1.getSerie();
+			List<String> series_list1 = actor1.getSerie();
 			List<Actor> actors_list1 = actors.getActor();
-			for(int i=0; i<value.size(); i++){
+			for (int i = 0; i < value.size(); i++) {
 				series_list1.add(value.get(i));
 			}
 			actors_list1.add(actor1);
-		
+
 		}
-		for (int i=0; i<actors.getActor().size(); i++){
+		for (int i = 0; i < actors.getActor().size(); i++) {
 			System.out.println(actors.getActor().get(i));
 		}
->>>>>>> Filipa
 		Boolean result = marshall.marshalles_actors(actors);
 		System.out.println(result);
 		if (!result) {
@@ -141,9 +120,4 @@ public class Processor {
 		}
 
 	}
-
-
-}
-
-	*/
 }
